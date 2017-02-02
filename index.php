b25fe94dd27fe621b97bb0e781dc6e2f720fef06
tingxuanz <zhangtx93@gmail.com>
2017-02-02 15:08:26 +1100

    bundle up codes for checking files into a function names checkFiles

--- index.php
+++ index.php
@@ -5,29 +5,38 @@
       $datasetNames = scandir($dir);
       $length = count($datasetNames);
 
-      for ($i=0; $i < $length ; $i++) {
-        if ($datasetNames[$i] !== '.' && $datasetNames[$i] !== '..') { //don't need link for directory . and ..
+      function checkFiles($dataSetName) {
+        //following are the file names that we expect
+        $metadataFile = $dataSetName . '_metadata.tsv';
+        $pcaFile = $dataSetName . '_pca.tsv';
+        $screetableFile = $dataSetName . '_screetable.tsv';
 
-          //following are the file names that we expect
-          $metadataFile = $datasetNames[$i] . '_metadata.tsv';
-          $pcaFile = $datasetNames[$i] . '_pca.tsv';
-          $screetableFile = $datasetNames[$i] . '_screetable.tsv';
+        //directory for each dataset
+        $directory = __DIR__ . '/data/' . $dataSetName . '/';
+        $pass = false;
 
-          //directory for each dataset
-          $directory = __DIR__ . '/data/' . $datasetNames[$i] . '/';
+        if (is_dir($directory)) {
+          $files = scandir($directory);
 
-          if (is_dir($directory)) {
-            $files = scandir($directory);
-
-            //The expected files must all exist, otherwise a link won't be created
-            if (in_array($metadataFile, $files, true)) {
-              if (in_array($pcaFile, $files, true)) {
-                if (in_array($screetableFile, $files, true)) {
-                  echo '<a target="_blank" href="../graph.php?ds_id=' . $datasetNames[$i] . '">' . $datasetNames[$i] . '</a>';
-                  echo '<br>';
-                }
+          //The expected files must all exist, otherwise a link won't be created
+          if (in_array($metadataFile, $files, true)) {
+            if (in_array($pcaFile, $files, true)) {
+              if (in_array($screetableFile, $files, true)) {
+                $pass = true;
               }
             }
+          }
+        }
+
+        return $pass;
+
+      }
+
+      for ($i=0; $i < $length ; $i++) {
+        if ($datasetNames[$i] !== '.' && $datasetNames[$i] !== '..') { //don't need link for directory . and ..
+          if (checkFiles($datasetNames[$i])) {
+            echo '<a target="_blank" href="../graph.php?ds_id=' . $datasetNames[$i] . '">' . $datasetNames[$i] . '</a>';
+            echo '<br>';
           }
         }
       }
