21265469e74700d74b046cdbc229efaee18bd563
tingxuanz <zhangtx93@gmail.com>
2017-02-02 14:17:26 +1100

    check files before creating links. If one of the three expected files doesn't exist, the link won't be created.

--- index.php
+++ index.php
@@ -7,8 +7,28 @@
 
       for ($i=0; $i < $length ; $i++) {
         if ($datasetNames[$i] !== '.' && $datasetNames[$i] !== '..') { //don't need link for directory . and ..
-          echo '<a target="_blank" href="../graph.php?ds_id=' . $datasetNames[$i] . '">' . $datasetNames[$i] . '</a>';
-          echo '<br>';
+
+          //following are the file names that we expect
+          $metadataFile = $datasetNames[$i] . '_metadata.tsv';
+          $pcaFile = $datasetNames[$i] . '_pca.tsv';
+          $screetableFile = $datasetNames[$i] . '_screetable.tsv';
+
+          //directory for each dataset
+          $directory = __DIR__ . '/data/' . $datasetNames[$i] . '/';
+
+          if (is_dir($directory)) {
+            $files = scandir($directory);
+
+            //The expected files must all exist, otherwise a link won't be created
+            if (in_array($metadataFile, $files, true)) {
+              if (in_array($pcaFile, $files, true)) {
+                if (in_array($screetableFile, $files, true)) {
+                  echo '<a target="_blank" href="../graph.php?ds_id=' . $datasetNames[$i] . '">' . $datasetNames[$i] . '</a>';
+                  echo '<br>';
+                }
+              }
+            }
+          }
         }
       }
     ?>
